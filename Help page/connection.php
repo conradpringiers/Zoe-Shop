3dfd58128914b1fdadef5f83660d7a9c3bd13de2
cydeb33 <46527845+cydeb33@users.noreply.github.com>
2022-04-30 11:04:39 +0200

    Update connection.php

--- Help page/connection.php
+++ Help page/connection.php
@@ -1,11 +1,14 @@
 <?php
     $user = 'root';
     $pass = 'root';
-        $connection = new PDO('mysql:host=localhost;port=3308;dbname=zoe', $user, $pass);
-        $query = "INSERT INTO `client` (`client_id`, `mail`, `address`, `postalcode`, `town`, `name`, `firstname`) VALUES ( 'hf', 't', '8975', 'ghguy', 'fh', 'hsr')";
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    $conn->exec($sql)
-
-
+        try {
+              $connection = new PDO('mysql:host=localhost;port=3308;dbname=zoe', $user, $pass);
+        $query = "INSERT INTO client ( mail, address, postalcode, town, name, firstname) VALUES ('hf', 't', '8975', 'ghguy', 'fh', 'hsr')";
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+              // use exec() because no results are returned
+              $conn->exec($sql);
+              echo "New record created successfully";
+            } catch(PDOException $e) {
+              echo $sql . "<br>" . $e->getMessage();
+            }
     ?>

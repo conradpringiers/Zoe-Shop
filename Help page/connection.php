2517ae5f2fc339c950f4b3e1abde039b0befd984
cyrille <cyrille.debongnie@gmail.com>
2022-05-06 11:25:08 +0200

    Update connection.php

--- Help page/connection.php
+++ Help page/connection.php
@@ -1,28 +1,33 @@
 <?php
     $user = 'root';
-    $pass = 'root';
-
+    $pass = '';
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => false
+    );
     $name =  $_REQUEST['name'];
     $telephone = $_REQUEST['telephone'];
     $subject =  $_REQUEST['subject'];
     $message = $_REQUEST['message'];
     $email = $_REQUEST['email'];
-    echo $name;
-    echo $telephone;
-    echo $subject;
-    echo $message;
-    echo $email;
-    /*
+    
     try {
-        $connection = new PDO('mysql:host=localhost;port=3308;dbname=zoe', $user, $pass);
-        $query = "INSERT INTO client ( mail, address, postalcode, town, name, firstname) VALUES ('hf', 't', '8975', 'ghguy', 'fh', 'hsr')";
-        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-          // use exec() because no results are returned
-        $conn->exec($sql);
-        echo "New record created successfully";
+        $connection = new PDO('mysql:host=localhost;dbname=zoe', $user, $pass, $options);
+        $query = "INSERT INTO `client` (`mail`, `address`, `postalcode`, `town`, `name`, `firstname`) VALUES (:mail, :address, :postalcode, :town, :name, :firstname)";
+        $statement = $connection->prepare($query);
+        $statement->bindValue(':mail', $email);
+        $statement->bindValue(':address', $subject);
+        $statement->bindValue(':postalcode', $telephone);
+        $statement->bindValue(':town', $message);
+        $statement->bindValue(':name', $name);
+        $statement->bindValue(':firstname', $name);
+        $statement->execute();
+        if($statement){
+            echo "New record created successfully";
+        }
+    
     } catch(PDOException $e) {
         echo $sql . "<br>" . $e->getMessage();
     }
 
-*/
 ?>

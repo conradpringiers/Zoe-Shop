b3d4136fb7da0757e776f49704c996d52fc3b89b
Syper80 <95524754+Syper80@users.noreply.github.com>
2022-05-18 18:11:47 +0200

    updated problem for data base
    problem from the 18 may

--- Help page/connection.php
+++ Help page/connection.php
@@ -5,22 +5,24 @@
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_EMULATE_PREPARES => false
     );
-    $name =  $_REQUEST['name'];
-    $telephone = $_REQUEST['telephone'];
-    $subject =  $_REQUEST['subject'];
-    $message = $_REQUEST['message'];
-    $email = $_REQUEST['email'];
+    $First_name =  $_REQUEST['First_name'];
+    $Second_name = $_REQUEST['Second_name'];
+    $Email =  $_REQUEST['Email'];
+    $Telephone = $_REQUEST['Telephone'];
+    $Subject = $_REQUEST['Subject'];
+    $Message = $_REQUEST['Message'];
     
     try {
         $connection = new PDO('mysql:host=localhost;dbname=zoe;port=8889', $user, $pass, $options);
-        $query = "INSERT INTO `client` (`mail`, `address`, `postalcode`, `town`, `name`, `firstname`) VALUES (:mail, :address, :postalcode, :town, :name, :firstname)";
+        $query = "INSERT INTO `client` (`First_name`, `Second_name`, `Email`, `Telephone`, `Subject`, `Message`) VALUES (:First_name, :Second_name, :Email, :Telephone, :Subject, :Message)";
+        
         $statement = $connection->prepare($query);
-        $statement->bindValue(':mail', $email);
-        $statement->bindValue(':address', $subject);
-        $statement->bindValue(':postalcode', $telephone);
-        $statement->bindValue(':town', $message);
-        $statement->bindValue(':name', $name);
-        $statement->bindValue(':firstname', $name);
+        $statement->bindValue(':First_name', $First_name);
+        $statement->bindValue(':Second_name', $Second_name);
+        $statement->bindValue(':Email', $Email);
+        $statement->bindValue(':Telephone', $Telephone);
+        $statement->bindValue(':Subject', $Subject);
+        $statement->bindValue(':Message', $Message);
         $statement->execute();
         if($statement){
             echo "New record created successfully";
